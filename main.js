822eb0c128279a53ddf933c8ac3a7e9dd4ea39a4
Jake McDermott <shakey1992jm@hotmail.co.uk>
2024-09-15 16:02:22 +0100

    feat: refactor the grid into an object to make the code cleaner and less DRY

--- main.js
+++ main.js
@@ -1,11 +1,42 @@
-const gridContainer = document.querySelector(".grid-container");
-const gridSize = 100;
-const numberOfGrids = gridSize * gridSize;
+const grid = {
+  gridContainer: document.querySelector(".grid-container"),
+  GenerateGridBtn: document.querySelector(".generate-grid-btn"),
+  defaultGridSize: 16,
+  maximumGridSize: 100,
 
-for (let i = 0; i < numberOfGrids; i++) {
-  const createGridItem = document.createElement("div");
-  createGridItem.classList.add("grid-item");
-  createGridItem.style.width = `calc(100%/${gridSize})`;
-  createGridItem.style.height = `calc(100%/${gridSize})`;
-  gridContainer.appendChild(createGridItem);
-}
+  createGrid(gridSize = this.defaultGridSize) {
+    this.gridContainer.innerHTML = ""; // Clear any previously created grids
+
+    // Dynamically create grids e.g a 16 x 16 grid repeats 256 times
+    for (let i = 0; i < gridSize * gridSize; i++) {
+      const createGridItem = document.createElement("div");
+      createGridItem.classList.add("grid-item");
+      createGridItem.style.width = `calc(100%/${gridSize})`;
+      createGridItem.style.height = `calc(100%/${gridSize})`;
+      this.gridContainer.appendChild(createGridItem);
+    }
+  },
+
+  validUserInput(input) {
+    //check value entered is a valid integer
+    const isInt = parseInt(input);
+    return !isNaN(isInt) ? true : false;
+  },
+
+  CreatenewGridFromPrompt() {
+    this.GenerateGridBtn.addEventListener("click", () => {
+      let userInput = prompt(
+        "Please enter a new size for the Etch-A-Sketch. Valid range is 1 - 100"
+      );
+      while (this.validUserInput(userInput) === false) {
+        userInput = prompt(
+          "Please enter a new size for the Etch-A-Sketch. Valid range is 1 - 100"
+        );
+      }
+      console.log(userInput);
+    });
+  },
+};
+
+grid.createGrid();
+grid.CreatenewGridFromPrompt();
